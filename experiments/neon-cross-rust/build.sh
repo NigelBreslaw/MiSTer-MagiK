f3b90580cc07517cf88d6bc81b7aee5b1c0ff535
Nigel Breslaw <nigel.breslaw@gmail.com>
2026-06-09 19:23:44 +0300

    Switch NEON experiment to pure Rust intrinsics

--- experiments/neon-cross-rust/build.sh
+++ experiments/neon-cross-rust/build.sh
@@ -5,5 +5,6 @@ cd "$(dirname "$0")"
 
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 export RUSTC_WRAPPER=""
+export RUSTFLAGS="${RUSTFLAGS:+$RUSTFLAGS }-C target-cpu=cortex-a9 -C target-feature=+neon"
 
 cross build --target armv7-unknown-linux-gnueabihf "$@"
