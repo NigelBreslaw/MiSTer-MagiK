43259562ee2ce3bce7c04e37d5b7e75582d0e257
Nigel Breslaw <nigel.breslaw@gmail.com>
2026-07-09 03:51:12 +0300

    Capture Quartus CI strace diagnostics

--- scripts/install-quartus-lite-docker.sh
+++ scripts/install-quartus-lite-docker.sh
@@ -122,7 +122,7 @@ FROM $BASE_IMAGE
 ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get update \
   && apt-get install -y --no-install-recommends \
-    bash ca-certificates file make perl python3 tar gzip xz-utils locales \
+    bash ca-certificates file make perl procps python3 strace tar gzip xz-utils locales \
     libc6-i386 lib32stdc++6 lib32z1 \
     libfontconfig1 libfreetype6 libglib2.0-0 libice6 libncurses5 libsm6 \
     libx11-6 libxau6 libxdmcp6 libxext6 libxft2 libxi6 libxrender1 libxt6 \
