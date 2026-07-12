49b709dd1e2931d8117a6d49fb2ba304e4537288
Nigel Breslaw <nigel.breslaw@gmail.com>
2026-07-12 05:06:39 +0300

    Avoid rebuilding durable catalog projections

--- scripts/profile-first-scan.sh
+++ scripts/profile-first-scan.sh
@@ -18,8 +18,8 @@ DEPLOY="skip"
 REPLACE_LABEL=0
 TIMEOUT_SECS=240
 SQLITE_BUILD_DIR=""
-RAM_CATALOG_READY_GATE_MS=106276
-DB_SAVE_GATE_MS=180778
+RAM_CATALOG_READY_GATE_MS=102738
+DB_SAVE_GATE_MS=128816
 source "$HERE/scripts/thread-sampler-lib.sh"
 source "$HERE/scripts/mister-supervision-lib.sh"
 
