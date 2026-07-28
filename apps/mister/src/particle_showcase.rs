ddd009adf5c5d2e1dd1dbaf659b120d8aa5be7be
Nigel Breslaw <nigel.breslaw@gmail.com>
2026-07-28 19:29:07 +0300

    Particles: reduce warp streak overdraw

--- apps/mister/src/particle_showcase.rs
+++ apps/mister/src/particle_showcase.rs
@@ -671,11 +671,11 @@ impl ParticleShowcaseRenderer {
         let visible = self.project_warp_speed_scalar(travel, previous_step);
 
         let stride = if speed < 0.12 {
-            64
+            96
         } else if speed < 0.55 {
-            16
+            32
         } else {
-            8
+            16
         };
         for index in (0..self.commands.len()).step_by(stride) {
             let current = self.commands[index];
